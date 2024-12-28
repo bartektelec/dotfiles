edc2a82b1943c0bb4f973b9c486b60d1b772d0e4
bartek <bartlomiej.telec@vipps.no>
2024-12-28 11:21:39 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,271 +1,343 @@
 local wezterm = require("wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
 
 local config = wezterm.config_builder()
+local session_manager = require("wezterm-session-manager/session-manager")
 
 config.color_scheme = "Dracula"
 config.font = wezterm.font("GohuFont uni11 Nerd Font Mono")
-config.font_size = 16
+config.font_size = 15
 config.enable_scroll_bar = true
 config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe", "-l" }
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 
 config.use_fancy_tab_bar = false
 
+-- wezterm.on("save_session", function(window)
+-- 	session_manager.save_state(window)
+-- end)
+-- wezterm.on("load_session", function(window)
+-- 	session_manager.load_state(window)
+-- end)
+-- wezterm.on("restore_session", function(window)
+-- 	session_manager.restore_state(window)
+-- end)
+
+resurrect.periodic_save({ interval_seconds = 600 })
+-- loads the state whenever I create a new workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+	local workspace_state = resurrect.workspace_state
+
+	workspace_state.restore_workspace(resurrect.load_state(label, "workspace"), {
+		window = window,
+		relative = true,
+		restore_text = true,
+		on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+	})
+end)
+
+-- Saves the state whenever I select a workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+	local workspace_state = resurrect.workspace_state
+	resurrect.save_state(workspace_state.get_workspace_state())
+end)
+
 wezterm.on("update-right-status", function(window, pane)
-  local left_cells = {}
-  local cells = {}
+	local left_cells = {}
+	local cells = {}
 
-  local cwd_uri = pane:get_current_working_dir()
-  if cwd_uri then
-    local cwd = ""
-    local hostname = ""
+	local cwd_uri = pane:get_current_working_dir()
+	if cwd_uri then
+		local cwd = ""
+		local hostname = ""
 
-    cwd = cwd_uri.file_path
-    hostname = cwd_uri.host or wezterm.hostname()
+		cwd = cwd_uri.file_path
+		hostname = cwd_uri.host or wezterm.hostname()
 
-    -- Remove the domain name portion of the hostname
-    local dot = hostname:find("[.]")
-    if dot then
-      hostname = hostname:sub(1, dot - 1)
-    end
-    if hostname == "" then
-      hostname = wezterm.hostname()
-    end
+		-- Remove the domain name portion of the hostname
+		local dot = hostname:find("[.]")
+		if dot then
+			hostname = hostname:sub(1, dot - 1)
+		end
+		if hostname == "" then
+			hostname = wezterm.hostname()
+		end
 
-    table.insert(left_cells, cwd)
-    table.insert(cells, hostname)
-  end
+		table.insert(left_cells, cwd)
+		table.insert(cells, hostname)
+	end
 
-  -- I like my date/time in this style: "Wed Mar 3 08:14"
-  local date = wezterm.strftime("%a %b %-d %H:%M")
-  table.insert(cells, date)
+	-- I like my date/time in this style: "Wed Mar 3 08:14"
+	local date = wezterm.strftime("%a %b %-d %H:%M")
+	table.insert(cells, date)
 
-  -- An entry for each battery (typically 0 or 1 battery)
-  for _, b in ipairs(wezterm.battery_info()) do
-    table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
-  end
+	-- An entry for each battery (typically 0 or 1 battery)
+	for _, b in ipairs(wezterm.battery_info()) do
+		table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+	end
 
-  -- The filled in variant of the < symbol
-  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	-- The filled in variant of the < symbol
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
-  -- Color palette for the backgrounds of each cell
-  local colors = {
-    "#3c1361",
-    "#52307c",
-    "#663a82",
-    "#7c5295",
-    "#b491c8",
-  }
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		"#3c1361",
+		"#52307c",
+		"#663a82",
+		"#7c5295",
+		"#b491c8",
+	}
 
-  -- Foreground color for the text across the fade
-  local text_fg = "#c0c0c0"
+	-- Foreground color for the text across the fade
+	local text_fg = "#c0c0c0"
 
-  -- The elements to be formatted
-  local elements = {}
-  local left_elements = {}
-  -- How many cells have been formatted
-  local num_cells = 0
+	-- The elements to be formatted
+	local elements = {}
+	local left_elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
 
-  -- Translate a cell into elements
-  function push(target, text, is_last)
-    local cell_no = num_cells + 1
-    table.insert(target, { Foreground = { Color = text_fg } })
-    table.insert(target, { Background = { Color = colors[cell_no] } })
-    table.insert(target, { Text = " " .. text .. " " })
-    if not is_last then
-      table.insert(target, { Foreground = { Color = colors[cell_no + 1] } })
-      table.insert(target, { Text = SOLID_LEFT_ARROW })
-    end
-    num_cells = num_cells + 1
-  end
+	-- Translate a cell into elements
+	function push(target, text, is_last)
+		local cell_no = num_cells + 1
+		table.insert(target, { Foreground = { Color = text_fg } })
+		table.insert(target, { Background = { Color = colors[cell_no] } })
+		table.insert(target, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(target, { Foreground = { Color = colors[cell_no + 1] } })
+			table.insert(target, { Text = SOLID_LEFT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
 
-  while #cells > 0 do
-    local cell = table.remove(cells, 1)
-    push(cell, #cells == 0)
-  end
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell, #cells == 0)
+	end
 
-  while #left_cells > 0 do
-    local cell = table.remove(left_cells, 1)
-    push(left_elements, cell, #cells == 0)
-  end
-  window:set_right_status(wezterm.format(elements))
-  window:set_left_status(wezterm.format(left_elements))
+	while #left_cells > 0 do
+		local cell = table.remove(left_cells, 1)
+		push(left_elements, cell, #cells == 0)
+	end
+	window:set_right_status(wezterm.format(elements))
+	window:set_left_status(wezterm.format(left_elements))
 end)
 config.colors = {
-  tab_bar = {
-    background = "#1D1E2E",
+	tab_bar = {
+		background = "#1D1E2E",
 
-    -- The active tab is the one that has focus in the window
-    active_tab = {
-      bg_color = "#313243",
-      fg_color = "#C0C8E5",
-      intensity = "Normal",
-      underline = "None",
-      italic = false,
-      strikethrough = false,
-    },
+		-- The active tab is the one that has focus in the window
+		active_tab = {
+			bg_color = "#313243",
+			fg_color = "#C0C8E5",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
 
-    inactive_tab = {
-      bg_color = "#1E1D2E",
-      fg_color = "#808080",
-    },
+		inactive_tab = {
+			bg_color = "#1E1D2E",
+			fg_color = "#808080",
+		},
 
-    new_tab = {
-      bg_color = "#1D1E2E",
-      fg_color = "#1D1E2E",
-    },
+		new_tab = {
+			bg_color = "#1D1E2E",
+			fg_color = "#1D1E2E",
+		},
 
-    new_tab_hover = {
-      bg_color = "#1D1E2E",
-      fg_color = "#909090",
-    },
-  },
+		new_tab_hover = {
+			bg_color = "#1D1E2E",
+			fg_color = "#909090",
+		},
+	},
 }
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 3000 }
 config.debug_key_events = true
 config.keys = {
-  {
-    key = "|",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "-",
-    mods = "LEADER",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "h",
-    mods = "LEADER",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "v",
-    mods = "LEADER",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "x",
-    mods = "LEADER",
-    action = wezterm.action.CloseCurrentPane({ confirm = true }),
-  },
-  {
-    key = "q",
-    mods = "LEADER",
-    action = wezterm.action.PaneSelect({}),
-  },
-  {
-    key = "RightArrow",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-  {
-    key = "LeftArrow",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "UpArrow",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Up"),
-  },
-  {
-    key = "DownArrow",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Down"),
-  },
-  {
-    key = "v",
-    mods = "CTRL",
-    action = wezterm.action.PasteFrom("Clipboard"),
-  },
-  {
-    key = "c",
-    mods = "LEADER",
-    action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-  },
-  {
-    key = "1",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(0),
-  },
-  {
-    key = "2",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(1),
-  },
-  {
-    key = "3",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(2),
-  },
-  {
-    key = "4",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(3),
-  },
-  {
-    key = "5",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(4),
-  },
-  {
-    key = ">",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    key = "<",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  {
-    key = "[",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  {
-    key = "]",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    key = "%",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.CloseCurrentTab({ confirm = true }),
-  },
-  {
-    key = "c",
-    mods = "CTRL",
-    action = wezterm.action.CopyTo("Clipboard"),
-  },
-  {
-    key = "w",
-    mods = "LEADER",
-    action = wezterm.action.ShowTabNavigator,
-  },
-  {
-    key = "s",
-    mods = "LEADER",
-    action = wezterm.action.ShowLauncherArgs({ flags = "WORKSPACES" }),
-  },
-  {
-    key = "p",
-    mods = "LEADER",
-    action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|COMMANDS" }),
-  },
-  {
-    key = "a",
-    mods = "LEADER|CTRL",
-    action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
-  },
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "v",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "q",
+		mods = "LEADER",
+		action = wezterm.action.PaneSelect({}),
+	},
+	{
+		key = "RightArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "LeftArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "UpArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "v",
+		mods = "CTRL",
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+	{
+		key = "c",
+		mods = "LEADER",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "1",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(0),
+	},
+	{
+		key = "2",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(1),
+	},
+	{
+		key = "3",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(2),
+	},
+	{
+		key = "4",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(3),
+	},
+	{
+		key = "5",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(4),
+	},
+	{
+		key = ">",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "<",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "]",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "%",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for session",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					wezterm.mux.rename_workspace(window:mux_window():get_workspace(), line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "s",
+		mods = "LEADER",
+		action = workspace_switcher.switch_workspace(),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|COMMANDS" }),
+	},
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{
+		key = "S",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.save_state(resurrect.workspace_state.get_workspace_state())
+		end),
+	},
+	{
+		key = "R",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_load(win, pane, function(id, label)
+				local type = string.match(id, "^([^/]+)") -- match before '/'
+				id = string.match(id, "([^/]+)$") -- match after '/'
+				id = string.match(id, "(.+)%..+$") -- remove file extention
+				local opts = {
+					relative = true,
+					restore_text = true,
+					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+				}
+				if type == "workspace" then
+					local state = resurrect.load_state(id, "workspace")
+					resurrect.workspace_state.restore_workspace(state, opts)
+				elseif type == "window" then
+					local state = resurrect.load_state(id, "window")
+					resurrect.window_state.restore_window(pane:window(), state, opts)
+				elseif type == "tab" then
+					local state = resurrect.load_state(id, "tab")
+					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+				end
+			end)
+		end),
+	},
 }
 return config
